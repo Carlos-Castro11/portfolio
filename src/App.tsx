02e8c52c9787aa41a72ca1ff1cfe585bcbd1e494
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-06-20 20:20:52 -0300

    Add shadcnui theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import './global.css'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | Carlos Castro" />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="carlos-castro-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | Carlos Castro" />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
