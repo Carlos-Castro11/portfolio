7a9ce11509332a7f90131f919d80f4a969a16ff6
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-06-20 18:45:37 -0300

    First Commit with ShadcnUi and Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
+export function App() {
+
+  return (
+    <div className='h-screen items-center justify-center flex'>
+      <Button>Enviar</Button>
+    </div>
+  )
+}
