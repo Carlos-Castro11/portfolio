1a56ae9c4ef776339d26264d32d1e666be42dbe2
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-06-29 20:09:03 -0300

    Add hotifx hero mobile

--- src/components/hero-mobile/hero-mobile.tsx
+++ src/components/hero-mobile/hero-mobile.tsx
@@ -23,39 +23,41 @@ export function HeroMobile() {
   })
 
   return (
-    <Card className="mt-4">
-      <div className="flex justify-center gap-2 md:gap-10">
-        <div className="text-center">
-          <img className="max-w-[150px] rounded-md" src={perfil} alt="" />
-          <CardDescription className="flex flex-col justify-center">
-            Desenvolvedor
-            <div>
-              {text}
-              <Cursor />
-            </div>
-          </CardDescription>
-        </div>
-        <div className="space-y-2 flex flex-col my-2 md:flex-none md:grid md:grid-cols-2 md:my-auto">
-          <HeroItem
-            icon={<Phone />}
-            fieldName="Telefone"
-            fieldValue="(38) 9 9128-5937"
-          />
-          <HeroItem
-            fieldName="Email"
-            fieldValue="carloshenriquemg2020@gmail.com"
-            icon={<Mail />}
-          />
-          <HeroItem
-            icon={<Calendar />}
-            fieldName="Data de nascimento"
-            fieldValue="24/04/2002"
-          />
-          <HeroItem
-            icon={<MapPin />}
-            fieldName="Localização"
-            fieldValue="Minas Gerais - BR"
-          />
+    <Card className="mt-2">
+      <div className="flex justify-center">
+        <div className="flex gap-1 sm:gap-6 md:gap-10">
+          <div className="text-center flex flex-col items-center">
+            <img className="max-w-[150px] rounded-md" src={perfil} alt="" />
+            <CardDescription className="flex flex-col justify-center">
+              Desenvolvedor
+              <div>
+                {text}
+                <Cursor />
+              </div>
+            </CardDescription>
+          </div>
+          <div className="flex-1 space-y-2 flex flex-col my-2 md:flex-none md:grid md:grid-cols-2 md:my-auto">
+            <HeroItem
+              icon={<Phone />}
+              fieldName="Telefone"
+              fieldValue="(38) 9 9128-5937"
+            />
+            <HeroItem
+              fieldName="Email"
+              fieldValue="carloshenriquemg2020@gmail.com"
+              icon={<Mail />}
+            />
+            <HeroItem
+              icon={<Calendar />}
+              fieldName="Data de nascimento"
+              fieldValue="24/04/2002"
+            />
+            <HeroItem
+              icon={<MapPin />}
+              fieldName="Localização"
+              fieldValue="Minas Gerais - BR"
+            />
+          </div>
         </div>
       </div>
       <Separator className="" />
