6ed6d9121584edf4b9d7d1c680ef2e8c629e5fe0
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-07-02 09:10:14 -0300

    Add new hero mobile layout

--- src/components/hero-mobile/hero-mobile.tsx
+++ src/components/hero-mobile/hero-mobile.tsx
@@ -1,18 +1,8 @@
-import {
-  Calendar,
-  GithubIcon,
-  LinkedinIcon,
-  Mail,
-  MapPin,
-  MessageCircleCodeIcon,
-  Phone,
-} from 'lucide-react'
+import { GithubIcon, LinkedinIcon, MessageCircleCodeIcon } from 'lucide-react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 
 import perfil from '../../assets/images/perfil.png'
-import { HeroItem } from '../hero/hero-item'
 import { Card, CardDescription } from '../ui/card'
-import { Separator } from '../ui/separator'
 import { HeroLinkMobile } from './hero-link-mobile'
 
 export function HeroMobile() {
@@ -43,35 +33,6 @@ export function HeroMobile() {
           <HeroLinkMobile icon={<LinkedinIcon />} text="Linkedin" />
           <HeroLinkMobile icon={<GithubIcon />} text="Github" />
           <HeroLinkMobile icon={<MessageCircleCodeIcon />} text="Whatsapp" />
-        </div>
-      </div>
-      <Separator />
-      <div className="flex items-center justify-center py-2">
-        <div className="flex gap-3">
-          <div className="space-y-2">
-            <HeroItem
-              icon={<Phone size={21} />}
-              fieldValue="(38) 9 9128-5937"
-              fieldName="Telefone"
-            />
-            <HeroItem
-              icon={<MapPin size={21} />}
-              fieldValue="Minas Gerais - Brasil"
-              fieldName="Localização"
-            />
-          </div>
-          <div className="space-y-2 justify-self-start">
-            <HeroItem
-              icon={<Mail size={21} />}
-              fieldValue="carloshenriquemg2020@gmail.com"
-              fieldName="E-mail"
-            />
-            <HeroItem
-              icon={<Calendar size={21} />}
-              fieldValue="24/04/2002"
-              fieldName="Data de nascimento"
-            />
-          </div>
         </div>
       </div>
     </Card>
