b4c557759d2c2f9f4b70ab6e179e46fd4ef77682
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-08-13 14:45:14 -0300

    Add new graph config

--- src/components/graphic.tsx
+++ src/components/graphic.tsx
@@ -1,85 +1,81 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import React from 'react'
-import Chart from 'react-apexcharts'
+import ApexChart from 'react-apexcharts'
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-class Graphic extends React.Component<{}, { options: any; series: any }> {
-  constructor(props: any) {
-    super(props)
+import { useTheme } from './theme/theme-provider'
 
-    this.state = {
-      options: {
-        dataLabels: {
-          enabled: false,
-        },
-        chart: {
-          id: 'basic-bar',
-          stacked: true,
-          toolbar: {
-            show: false,
-          },
-        },
-        plotOptions: {
-          bar: {
-            borderRadius: 5,
-            columnWidth: '40%',
-          },
-        },
-        xaxis: {
-          categories: ['Python', 'Tailwind', 'Typescript', 'C#'],
-        },
-        yaxis: {
-          show: true,
-          min: 1,
-          max: 10,
+export function Graphic() {
+  const { theme } = useTheme()
+
+  const primary = theme === 'dark' ? '#fff' : '#000'
+
+  const options: ApexCharts.ApexOptions | undefined = {
+    tooltip: {
+      enabled: false,
+    },
+    xaxis: {
+      categories: ['React.js', 'Tailwind', 'Typescript', 'C#'],
+      labels: {
+        style: {
+          colors: primary,
         },
       },
-      series: [
-        {
-          data: [
-            {
-              x: 'Python',
-              y: 6,
-              fillColor: '#ffe',
-            },
-            {
-              x: 'Category B',
-              y: 7,
-              fillColor: '#ffe',
-            },
-            {
-              x: 'Category C',
-              y: 9,
-              fillColor: '#ffe',
-            },
-            {
-              x: 'Category C',
-              y: 8,
-              fillColor: '#ffe',
-            },
-          ],
-        },
-      ],
-    }
+    },
+    yaxis: {
+      min: 1,
+      max: 10,
+      labels: {
+        show: false,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    chart: {
+      id: 'basic-bar',
+      stacked: true,
+      toolbar: {
+        show: false,
+      },
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 5,
+        columnWidth: '40%',
+      },
+    },
   }
 
-  render() {
-    return (
-      <div className="app">
-        <div className="row">
-          <div className="mixed-chart">
-            <Chart
-              options={this.state.options}
-              series={this.state.series}
-              type="bar"
-              width="325"
-              strokeColor="#775DD0"
-            />
-          </div>
-        </div>
-      </div>
-    )
-  }
+  const series = [
+    {
+      data: [
+        {
+          x: 'teste',
+          y: [5],
+          fillColor: primary,
+        },
+        {
+          x: 'teste2',
+          y: [7],
+          fillColor: primary,
+        },
+        {
+          x: 'teste3',
+          y: [4],
+          fillColor: primary,
+        },
+        {
+          x: 'teste4',
+          y: [9],
+          fillColor: primary,
+        },
+      ],
+    },
+  ]
+
+  return (
+    <div className="">
+      <ApexChart options={options} series={series} type="bar" width="350" />
+    </div>
+  )
 }
 
 export default Graphic
