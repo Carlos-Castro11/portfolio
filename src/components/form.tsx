b3ca092d11c0d4c48cebd66427fca831e385d35a
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-08-14 15:57:14 -0300

    Add e-mail sending function

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,3 +1,4 @@
+import emailjs from '@emailjs/browser'
 import { useForm } from 'react-hook-form'
 import { toast, Toaster } from 'sonner'
 import { z } from 'zod'
@@ -8,23 +9,42 @@ import { Label } from './ui/label'
 import { Textarea } from './ui/textarea'
 
 const contactForm = z.object({
-  subject: z.string(),
-  email: z.string(),
+  name: z.string(),
+  email: z.string().email(),
+  message: z.string(),
 })
 
 type ContactForm = z.infer<typeof contactForm>
 
 export function Form() {
-  const { register, handleSubmit } = useForm<ContactForm>({
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitting },
+  } = useForm<ContactForm>({
     defaultValues: {
-      subject: '',
+      name: '',
       email: '',
+      message: '',
     },
   })
 
   async function handleSendEmail(data: ContactForm) {
+    const templateParams = {
+      from_name: data.name,
+      message: data.message,
+      email: data.email,
+    }
     try {
+      await emailjs.send(
+        'service_d4zwoyt',
+        'template_3h2j3rh',
+        templateParams,
+        'hPq8qTBzrcSO6AV2B',
+      )
       toast.success('E-mail enviado com sucesso!')
+      reset()
     } catch {
       toast.error('Ocorreu um erro...')
     }
@@ -37,25 +57,50 @@ export function Form() {
         className="border rounded-md p-3 space-y-3"
         onSubmit={handleSubmit(handleSendEmail)}
       >
-        <h3>Formulário para contato</h3>
-        <div>
-          <Label htmlFor="subject">Assunto</Label>
-          <Input
-            {...register('subject')}
-            id="subject"
-            type="text"
-            placeholder="Ex: Orçamento, Oferta, Indicação..."
-          />
+        <h3>Entre em contato comigo!</h3>
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <Label htmlFor="name">Nome</Label>
+            <Input
+              required={true}
+              minLength={5}
+              className=""
+              {...register('name', { required: true, minLength: 5 })}
+              id="name"
+              type="text"
+              placeholder="Digite seu nome..."
+            />
+          </div>
+          <div>
+            <Label htmlFor="email">E-mail</Label>
+            <Input
+              required={true}
+              className=""
+              {...register('email', { required: true })}
+              id="email"
+              type="email"
+              placeholder="Digite seu e-mail..."
+            />
+          </div>
         </div>
         <div>
-          <Label htmlFor="email">E-mail</Label>
+          <Label htmlFor="message">Mensagem</Label>
           <Textarea
-            {...register('email')}
-            id="email"
+            required={true}
+            minLength={15}
+            {...register('message', { required: true })}
+            id="message"
             placeholder="A sua mensagem aqui."
           />
         </div>
-        <Button type="submit">Enviar</Button>
+        <Button
+          className={`${isSubmitting && 'cursor-wait'}`}
+          disabled={isSubmitting}
+          type="submit"
+          variant={isSubmitting ? 'outline' : 'default'}
+        >
+          {isSubmitting ? 'Enviando...' : 'Enviar'}
+        </Button>
       </form>
     </>
   )
